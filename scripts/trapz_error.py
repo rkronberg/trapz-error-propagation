32bfa049b79f0887ff08051bd38223497c9e626f
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-24 01:09:37 +0300

    Commented the code

--- scripts/trapz_error.py
+++ scripts/trapz_error.py
@@ -13,18 +13,24 @@ import numpy as np
 class Trapz:
 	def __init__(self, x):
 
+		# Calculate grid spacing
 		self.dx = np.diff(x)
 
 	def integrate(self, y, init=0.0):
-	
+		
 		i = np.arange(len(y)-1)
+
+		# Cumulative trapezoidal rule
 		integral = 0.5*np.cumsum((y[i]+y[i+1])*self.dx)
 		self.integral = np.concatenate(([init], integral))
 	
 	def propagate(self, yerr):
-	
+		
 		i = np.arange(len(yerr)-2)
+
+		# Single grid spacing at boundaries, two-step elsewhere
 		ddx = self.dx[i]+self.dx[i+1]
 		ddx = np.concatenate(([self.dx[0]], ddx, [self.dx[-1]]))
-	
+
+		# Error propagation of a weighted sum
 		self.error = np.sqrt(0.25*np.cumsum((yerr*ddx)**2))
