d2d7432eaacf00f7bd0e9ac9dd0889dec161a5bd
Rasmus <rasmus.kronberg@aalto.fi>
2020-09-03 12:08:19 +0300

    Style fixes

--- scripts/main.py
+++ scripts/main.py
@@ -3,50 +3,61 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 from trapz_error import Trapz
 
+
 def parse():
 
-	# Parse command line arguments
+    # Parse command line arguments
 
-	p = ArgumentParser(description='Thermodynamic integration code')
-	p.add_argument('-i', '--input', required=True, help='Input file')
-	p.add_argument('-I', '--initial', type=float, default=0.0, help='Initial value of integral')
-	p.add_argument('-p', '--plot', action='store_true', help='Visualize results')
+    p = ArgumentParser(description='Thermodynamic integration code')
+    p.add_argument('-i', '--input', required=True, help='Input file')
+    p.add_argument('-I', '--initial', type=float, default=0.0,
+                   help='Initial value of integral')
+    p.add_argument('-p', '--plot', action='store_true',
+                   help='Visualize results')
 
-	return vars(p.parse_args())
+    return vars(p.parse_args())
+
 
 def main():
 
-	args = parse()
-	inp = args['input']
-	init = args['initial']
-	isplot = args['plot']
+    args = parse()
+    inp = args['input']
+    init = args['initial']
+    isplot = args['plot']
 
-	# Expects input shape (n, m) where n is the number 
-	# of samples and m = 3 (xdata, ydata, yerror)
-	try:
-		xdata = np.loadtxt(inp, usecols=[0])
-		ydata = np.loadtxt(inp, usecols=[1])
-		yerror = np.loadtxt(inp, usecols=[2])
-	except:
-		print('Check input dims. Shape (n, 3) containing xdata, ydata and yerror required.')
+    # Expects input shape (n, m) where n is the number
+    # of samples and m = 3 (xdata, ydata, yerror)
+    try:
+        xdata = np.loadtxt(inp, usecols=[0])
+        ydata = np.loadtxt(inp, usecols=[1])
+        yerror = np.loadtxt(inp, usecols=[2])
+    except IndexError:
+        print('Check input dims. Shape (n, 3) containing xdata, \
+            ydata and yerror required.')
+        quit()
+    except OSError:
+        print('Input file not found.')
+        quit()
 
-	# Initialize by calculating grid spacing based on xdata
-	tz = Trapz(ydata, x=xdata)
+    # Initialize by calculating grid spacing based on xdata
+    tz = Trapz(ydata, x=xdata)
 
-	# Integrate using the cumulative trapezoidal rule
-	tz.integrate(init)
+    # Integrate using the cumulative trapezoidal rule
+    tz.integrate(init)
 
-	# Propagate errors associated with input ydata
-	tz.propagate(yerror)
+    # Propagate errors associated with input ydata
+    tz.propagate(yerror)
 
-	np.savetxt('integral.out', np.c_[xdata, tz.integral, tz.error], header='x, F(x), error')
+    np.savetxt('integral.out', np.c_[xdata, tz.integral, tz.error],
+               header='x, F(x), error')
 
-	# Visualize results
-	if(isplot):
-		plt.plot(xdata, tz.integral, '.-')
-		plt.fill_between(xdata, tz.integral-tz.error, tz.integral+tz.error, alpha=0.25)
-		plt.show()
+    # Visualize results
+    if(isplot):
+        plt.plot(xdata, tz.integral, '.-')
+        plt.fill_between(xdata, tz.integral-tz.error, tz.integral+tz.error,
+                         alpha=0.25)
+        plt.show()
+
 
 if __name__ == '__main__':
-	main()
-	
+    main()
--- scripts/trapz_error.py
+++ scripts/trapz_error.py
@@ -1,6 +1,6 @@
 """
-Class for performing numerical integration using the 
-cumulative (composite) trapezoidal rule. Error bars 
+Class for performing numerical integration using the
+cumulative (composite) trapezoidal rule. Error bars
 are computed based on standard uncertainty propagation.
 
 author: Rasmus Kronberg
@@ -10,35 +10,36 @@ email: rasmus.kronberg@aalto.fi
 
 import numpy as np
 
+
 class Trapz:
-	def __init__(self, y, x=None, dx=1.0):
+    def __init__(self, y, x=None, dx=1.0):
 
-		self.y = y
+        self.y = y
 
-		# Calculate grid spacing
-		if x is not None:
-			self.dx = np.diff(x)
-		else:
-			self.dx = dx*np.ones(len(y))
+        # Calculate grid spacing
+        if x is not None:
+            self.dx = np.diff(x)
+        else:
+            self.dx = dx*np.ones(len(y))
 
-	def integrate(self, init=0.0):
-		
-		y = self.y
-		dx = self.dx
-		i = np.arange(len(y)-1)
+    def integrate(self, init=0.0):
 
-		# Cumulative trapezoidal rule
-		integral = 0.5*np.cumsum((y[i]+y[i+1])*dx)
-		self.integral = np.concatenate(([init], integral))
-	
-	def propagate(self, yerr):
-		
-		dx = self.dx
-		i = np.arange(len(yerr)-2)
+        y = self.y
+        dx = self.dx
+        i = np.arange(len(y)-1)
 
-		# Single grid spacing at boundaries, two-step elsewhere
-		ddx = dx[i]+dx[i+1]
-		ddx = np.concatenate(([dx[0]], ddx, [dx[-1]]))
+        # Cumulative trapezoidal rule
+        integral = 0.5*np.cumsum((y[i]+y[i+1])*dx)
+        self.integral = np.concatenate(([init], integral))
 
-		# Error propagation of a weighted sum
-		self.error = np.sqrt(0.25*np.cumsum((yerr*ddx)**2))
+    def propagate(self, yerr):
+
+        dx = self.dx
+        i = np.arange(len(yerr)-2)
+
+        # Single grid spacing at boundaries, two-step elsewhere
+        ddx = dx[i]+dx[i+1]
+        ddx = np.concatenate(([dx[0]], ddx, [dx[-1]]))
+
+        # Error propagation of a weighted sum
+        self.error = np.sqrt(0.25*np.cumsum((yerr*ddx)**2))
